a00068f55e6aaba792785a01f1d10a7fe6d85fab
Mika Vainio <koti.mika.vainio@gmail.com>
2025-01-15 09:29:10 +0200

    Luotu testirunko tietokantamoduulille

---
+++ test_dbOperations.py
@@ -0,0 +1,13 @@
+# TIETOKANTAYHTEYKSIEN TESTAUS
+# ============================
+
+import pytest # Virheilmoitusten testaus vaatii
+import dbOperations # Testattava moduuli
+
+# TODO: Luo tietokantayhteys-objekti testejä varten
+
+# TODO: Testaa, että yhteysmerkkijono muodostuu oikein
+
+# TODO: Testaa että taulun kaikki tiedot saadaan
+
+# TODO: Mieti mitä muita testejä pitää kirjoittaa
